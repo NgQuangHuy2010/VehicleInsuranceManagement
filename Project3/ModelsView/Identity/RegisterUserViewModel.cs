36edfdffb4d755e285147d76b52b313b6e69a32d
Huy <nqht123456789@gmail.com>
2024-08-02 19:04:05 +0700

    feat : add email , register confirm email

---
+++ Project3/ModelsView/Identity/RegisterUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project3.ModelsView.Identity
+{
+    public class RegisterUserViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Fullname { get; set; }
+
+        public string? Phone { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Confirm password does not match!!!")]
+        public string? ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        public string? EmailConfirmationCode { get; set; }
+    }
+}
