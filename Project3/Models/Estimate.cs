a88b3e870bf76af257be2a1fac670eee9349b78b
Huy <nqht123456789@gmail.com>
2024-08-02 19:03:23 +0700

    Resolve merge conflicts

--- Project3/Models/Estimate.cs
+++ Project3/Models/Estimate.cs
@@ -1,27 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Project3.Models;
-
-public partial class Estimate
+﻿namespace Project3.Models
 {
-    public int Id { get; set; }
+    public partial class Estimate
+    {
+        public int Id { get; set; }
 
-    public int? CustomerId { get; set; }
+        public int? CustomerId { get; set; }
 
-    public decimal? EstimateNumber { get; set; }
+        public decimal? EstimateNumber { get; set; }
 
-    public string? CustomerName { get; set; }
+        public string? CustomerName { get; set; }
 
-    public decimal? CustomerPhoneNumber { get; set; }
+        public decimal? CustomerPhoneNumber { get; set; }
 
-    public string? VehicleName { get; set; }
+        public string? VehicleName { get; set; }
 
-    public string? VehicleModel { get; set; }
+        public string? VehicleModel { get; set; }
 
-    public decimal? VehicleRate { get; set; }
+        public string? VehicleVersion { get; set; }
 
-    public string? VehicleWarranty { get; set; }
+        public decimal? VehicleRate { get; set; }
 
-    public string? VehiclePolicyType { get; set; }
+        public string? VehicleBodyNumber { get; set; }
+
+        public string? VehicleEngineNumber { get; set; }
+
+        public decimal? VehicleNumber { get; set; }
+
+        public string? VehicleWarranty { get; set; }
+
+        public string? VehiclePolicyType { get; set; }
+
+        // Additional Properties
+        public int DriverAge { get; set; }
+
+        public string DriverGender { get; set; }
+
+        public string Location { get; set; }
+
+        public string Usage { get; set; }
+
+        public bool AntiTheftDevice { get; set; }
+
+        public List<string> SelectedCoverages { get; set; }
+
+        public int DrivingHistory { get; set; }
+
+        public bool? MultiPolicy { get; set; }
+        public bool? SafeDriver { get; set; }
+
+        public decimal EstimatedCost { get; set; }
+        public virtual AspNetUser? AspNetUser { get; set; }
+    }
 }
--- Project3/Models/VehicleInformation.cs
+++ Project3/Models/VehicleInformation.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Project3.Models;
-
-public partial class VehicleInformation
+﻿namespace Project3.Models
 {
-    public int Id { get; set; }
+    public partial class VehicleInformation
+    {
+        public int Id { get; set; }
 
-    public string? VehicleName { get; set; }
+        public string? VehicleName { get; set; }
 
-    public string? VehicleOwnerName { get; set; }
+        public string? VehicleOwnerName { get; set; }
 
-    public string? VehicleModel { get; set; }
+        public string? VehicleModel { get; set; }
 
-    public string? VehicleVersion { get; set; }
+        public string? VehicleVersion { get; set; }
 
-    public decimal? VehicleRate { get; set; }
+        public decimal? VehicleRate { get; set; }
 
-    public string? VehicleBodyNumber { get; set; }
+        public string? VehicleBodyNumber { get; set; }
 
-    public string? VehicleEngineNumber { get; set; }
+        public string? VehicleEngineNumber { get; set; }
 
-    public decimal? VehicleNumber { get; set; }
+        public decimal? VehicleNumber { get; set; }
+        public string? Location { get; set; } // New field
+        public string? Usage { get; set; } // New field
+        public string? WarrantyType { get; set; } // New field
+        public string? PolicyType { get; set; } // New field
+    }
 }
