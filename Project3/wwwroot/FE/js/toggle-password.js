9a5d78c7cc74f2564372ee59f350820b3412baaa
Huy <nqht123456789@gmail.com>
2024-08-04 23:43:49 +0700

    feat : add profile user , change password, validation email and phone , fullname

--- Project3/wwwroot/FE/js/toggle-password.js
+++ Project3/wwwroot/FE/js/toggle-password.js
@@ -1,13 +1,18 @@
 // site.js
 document.addEventListener('DOMContentLoaded', function () {
+    const current = document.getElementById("current");
+    const showCurrent = document.getElementById("showCurrent");
+    showCurrent.onchange = function (e) {
+        current.type = showCurrent.checked ? "text" : "password";
+    };
+});
+document.addEventListener('DOMContentLoaded', function () {
     const pwd = document.getElementById("password");
     const chk = document.getElementById("showPassword");
     chk.onchange = function (e) {
         pwd.type = chk.checked ? "text" : "password";
     };
 });
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const confirm = document.getElementById("confirm");
     const showconfirm = document.getElementById("showConfirm");
@@ -15,3 +20,6 @@ document.addEventListener('DOMContentLoaded', function () {
         confirm.type = showconfirm.checked ? "text" : "password";
     };
 });
+
+
+
