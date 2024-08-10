35d36d10ac7f957417b0451cc3f181e3c47175cf
chuongtruong96 <139564688+chuongtruong96@users.noreply.github.com>
2024-08-10 10:57:22 +0700

    using

--- Project3/Areas/System/Controllers/AdminInsuranceProcessController.cs
+++ Project3/Areas/System/Controllers/AdminInsuranceProcessController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Project3.Models;
 using Project3.ModelsView;
 using Project3.ModelsView.Identity;
